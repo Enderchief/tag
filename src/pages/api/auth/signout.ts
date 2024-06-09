5d62dc9855d6cbfd7d108277c6f7314c1af64b39
Endercheif <reet22singh@gmail.com>
2024-06-08 21:54:58 -0700

    dev: debug info in query string

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async ({ cookies, redirect }) => {
+export const GET: APIRoute = async ({ cookies, redirect, url }) => {
 	cookies.delete('sb-access-token', { path: '/' });
 	cookies.delete('sb-refresh-token', { path: '/' });
-	return redirect('/');
+	return redirect('/' + url.search);
 };

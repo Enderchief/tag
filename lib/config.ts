61e32b99bc5deb9863f80788723798c3a39da94e
Enderchief <45527309+Enderchief@users.noreply.github.com>
2024-07-12 19:28:41 -0700

    fix: added env to config file

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+export default {
+  EXPO_PUBLIC_DB_URL: "https://mueognnnszklizwshvdc.supabase.co",
+  EXPO_PUBLIC_DB_PUBLIC_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11ZW9nbm5uc3prbGl6d3NodmRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY1MDQxODQsImV4cCI6MjAzMjA4MDE4NH0.3iunB9GlAPjGExBsVx1-MUCDyb_0aulcp_hz197Y4_A",
+};
--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,24 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from './types';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+import config from "./config";
+import type { Database } from "./types";
 
-type Rows<T extends keyof Database['public']['Tables']> =
-	Database['public']['Tables'][T]['Row'];
+type Rows<T extends keyof Database["public"]["Tables"]> =
+  Database["public"]["Tables"][T]["Row"];
 
-export type User = Rows<'user'>;
-export type Challenges = Rows<'challenges'>;
-export type Team = Rows<'team'>;
+export type User = Rows<"user">;
+export type Challenges = Rows<"challenges">;
+export type Team = Rows<"team">;
 
 export const supabase = createClient<Database>(
-	process.env.EXPO_PUBLIC_DB_URL!,
-	process.env.EXPO_PUBLIC_DB_PUBLIC_KEY!,
-	{
-		auth: {
-			storage: AsyncStorage,
-			autoRefreshToken: true,
-			persistSession: true,
-			detectSessionInUrl: false,
-		},
-	}
+  config.EXPO_PUBLIC_DB_URL,
+  config.EXPO_PUBLIC_DB_PUBLIC_KEY,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  },
 );
